d2ad8ce9009d71fb609a770ae124e3a89fb91974
bayorwor <bayorwor60@gmail.com>
2022-03-25 17:25:43 +0000

    added components

---
+++ .history/src/App_20220325171817.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import ProfileForm from "./components/ProfileForm";
+import ProfileCard from "./components/ProfileCard";
+
+function App() {
+  const [allProfiles, setAllProfiles] = useState([
+    {
+      firstName: "Gbemu",
+      lastName: "Terra",
+      email: "gbemu@google.com",
+      phone: "0240123456",
+    },
+  ]);
+
+  const updateProfiles = (profile) => {
+    let arr = allProfiles;
+    arr.push(profile);
+    setAllProfiles([...arr]);
+  };
+
+  return (
+    <div className="app">
+      <h1> Profile Maker </h1>
+      <div>
+        <ProfileForm submit={updateProfiles} />
+        <hr />
+        <div className="list">
+          {allProfiles.map((person, index) => (
+            <ProfileCard key={index} card={person} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
